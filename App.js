27cfeaf2ab83b28934c6c068082a3e0a2e761ffd
Ana Laura <anynnha.laureca@gmail.com>
2021-02-01 22:10:37 -0300

    Adicionado ESlint e estilização da logo e botões

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{uri: 'https://i.ibb.co/zsD9Zyw/logo-compelta.png'}}
-      />
-    </View>
-  );
-}
+import logo from './images/logo-completa.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,8 +10,56 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   logo: {
     height: 110,
     width: 210,
   },
+
+  btnEntrar: {
+    height: 40,
+    width: 330,
+    backgroundColor: '#242424',
+    borderRadius: 10,
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '32px 0',
+  },
+  
+  btnCadastrar: {
+    height: 40,
+    width: 330,
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: '#242424',
+    borderRadius: 10,
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image  
+        style={styles.logo}
+        source={logo}
+      />
+      <TouchableOpacity
+        style={styles.btnEntrar}
+        onPress={() => Alert.alert('Simple Button pressed')}
+      >
+        <Text style={{textAlign: 'center'}}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btnCadastrar}
+        onPress={() => Alert.alert('Simple Button pressed')}
+      >
+        <Text style={{textAlign: 'center'}}>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
