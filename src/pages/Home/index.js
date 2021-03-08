74437e212500f5ce2247a307b5745d9eb64a68ec
Ana Laura <anynnha.laureca@gmail.com>
2021-03-08 19:41:00 -0300

    Criando a página de home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F2D43A',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: "column",
+    },
+});
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
