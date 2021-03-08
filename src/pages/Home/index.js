46d6914720bb0daab67fca99c82431d23b1ee5a7
Ana Laura <anynnha.laureca@gmail.com>
2021-03-08 20:02:08 -0300

    Inserir a logo na página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {  Avatar, Button, Card, Title, StyleSheet, View, Image } from 'react-native';
+
+import logo from '../../assets/images/completa-amarela.png'
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#F2D43A',
+      backgroundColor: 'white',
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: "column",
+    },
+    logo: {
+        height: 40,
+        width: 75,
     },
 });
 
 export default function Home() {
     return (
         <View style={styles.container}>
-
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                width: '100%',
+                alignItems: 'center',
+            }}>
+                <Image
+                    source={logo}
+                    style={styles.logo}
+                />
+            </View>
         </View>
     );
 }
